ecc3d23cf69aba479763ed78371fdf9379d4149f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-30 20:27:40 -0300

    organizando

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,13 +5,10 @@ import 'package:checkpoint05_webservices_rm550347/main.dart';
 
 void main() {
   testWidgets('App starts and shows title', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verifica se o título do AppBar está sendo exibido.
     expect(find.text('Minhas Receitas'), findsOneWidget);
-    
-    // Verifica se o FloatingActionButton (botão de adicionar) está presente.
+
     expect(find.byIcon(Icons.add), findsOneWidget);
   });
 }
